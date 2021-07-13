86f3f874049785c6461eb95be2a7c07ffbad41c1
ViniciusRoosevelt <84464600+ViniciusRoosevelt@users.noreply.github.com>
2021-07-13 19:35:38 -0300

    Add files via upload

---
+++ lista 4 - for/1.js
@@ -0,0 +1,11 @@
+/* Leia N e escreva todos os números inteiros de 1 a N. */
+function main(){
+    console.log('Todos os naturais até n')
+    const prompt = require('prompt-sync')()
+    const n = Number(prompt('Insira um número'))
+    for(let i = 1 ; i <= n ; i++){
+        if(i > 100){
+            break}
+        console.log(`Os naturais de 1 até ${n} são: ${i}`)
+    }
+}main()
