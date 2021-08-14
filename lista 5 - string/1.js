c9ead7c17588bf4253d91853ea1c058540cc1158
ViniciusRoosevelt <84464600+ViniciusRoosevelt@users.noreply.github.com>
2021-08-14 18:46:54 -0300

    Delete lista 5 - string directory

--- lista 5 - string/1.js
+++
@@ -1,7 +0,0 @@
-const entrada = require('prompt-sync')()
-function main(){
-    console.log('### SEGREDO ###')
-    let texto = entrada('Insira um texto para criptografar')
-    texto = texto.split('').reverse('').join('')
-    console.log(texto)
-}main()
--- lista 5 - string/2.js
+++
@@ -1,9 +0,0 @@
-/*Leia uma frase e mostre cada palavra da frase em uma linha separada. */
-const prompt = require('prompt-sync')()
-function main(){
-    console.log('### Texto e Enumeração ###')
-    const texto = prompt('Insira um texto: ')
-    for(let i = 0 ; i < texto.length ; i++){
-        console.log(`A frase dividida é ${texto[i]}`)
-    }
-}main()
