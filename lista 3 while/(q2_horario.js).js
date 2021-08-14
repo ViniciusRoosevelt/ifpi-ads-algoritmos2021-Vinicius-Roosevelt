3b3b355d908273ec2991f2ac09c4a2a24e53f533
ViniciusRoosevelt <84464600+ViniciusRoosevelt@users.noreply.github.com>
2021-08-14 18:46:42 -0300

    Delete lista 3 while directory

--- lista 3 while/(q2_horario.js).js
+++
@@ -1,7 +0,0 @@
-/* Leia um horário no formato hh:mm:ss e escreva o resultado na
-seguinte forma: “hh hora(s), mm minuto(s) e ss segundo(s) */
-const prompt = require('prompt-sync')()
-function tempo(){
-    const t = prompt('Insira um horário no formato,hh:mm:ss :')
-    console.log(`${t.length[0]}${t.length[1]}horas`)
-}
--- lista 3 while/6.js
+++
@@ -1,7 +0,0 @@
-/* Escreva um algoritmo para determinar o número de dígitos de um número informado. */
-const prompt = require('prompt-sync')()
-function main(){
-    console.log('### Algarismo dos Números ####')
-    const n = Number(prompt('Insira um número: '))
-    
-}
