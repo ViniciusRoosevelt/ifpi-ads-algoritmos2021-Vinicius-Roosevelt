f6807fe93fba1c32042fdd58d7001487511f7ccf
ViniciusRoosevelt <84464600+ViniciusRoosevelt@users.noreply.github.com>
2022-01-05 19:49:20 -0300

    Add files via upload

--- viniciusflix/script.js
+++ viniciusflix/script.js
@@ -6,19 +6,202 @@ const API_URL = BASE_URL+ '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL +'/search/movie?'+API_KEY;
 
+const genres = [
+    {
+      "id": 28,
+      "name": "Ação"
+    },
+    {
+      "id": 12,
+      "name": "Aventura"
+    },
+    {
+      "id": 16,
+      "name": "Animação"
+    },
+    {
+      "id": 35,
+      "name": "Comédia"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentário"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Família"
+    },
+    {
+      "id": 14,
+      "name": "Fantasia"
+    },
+    {
+      "id": 36,
+      "name": "História"
+    },
+    {
+      "id": 27,
+      "name": "Terror"
+    },
+    {
+      "id": 10402,
+      "name": "Música"
+    },
+    {
+      "id": 9648,
+      "name": "Mistério"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Ficção científica"
+    },
+    {
+      "id": 10770,
+      "name": "Cinema TV"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "Guerra"
+    },
+    {
+      "id": 37,
+      "name": "Faroeste"
+    }
+  ]
+       
+
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const tagsEl = document.getElementById('tags');
 
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next'); 
+const current = document.getElementById('current');
+
+
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = '';
+var totalPages = 100;
+
+
+var selectedGenre = []
+
+setGenre()
+function setGenre(){
+    tagsEl.innerHTML = '';
+    genres.forEach(genre => {
+        const t = document.createElement('div');
+        t.classList.add('tag');
+        t.id=genre.id;
+        t.innerText= genre.name;
+        t.addEventListener('click',()=>{
+            if(selectedGenre.length == 0){
+                selectedGenre.push(genre.id);
+            }else{
+                if(selectedGenre.includes(genre.id)){
+                    selectedGenre.forEach((id,idx) =>{
+                        if(id == genre.id){
+                            selectedGenre.splice(idx, 1)
+                        }
+                    })
+                }else{
+                    selectedGenre.push(genre.id);
+                }
+            }
+            getMovies(API_URL+ '&with_genres='+encodeURI(selectedGenre.join(',')))
+            highlightSelect()
+        })
+        tagsEl.append(t);
+    })
+}
+function highlightSelect(){
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach(tag =>{
+        tag.classList.remove('highlight')
+    })
+    clearBtn()
+    if(selectedGenre.length != 0){
+        selectedGenre.forEach(id =>{
+            const highlightSelectTag = document.getElementById(id);
+            highlightSelectTag.classList.add('highlight');
+        })
+    }
+
+    
+}
+function clearBtn(){
+
+
+    let clearBtn = document.getElementById('clear');
+    if(clearBtn){
+        clearBtn.classList.add('highlight')
+    }else{
+        let clear = document.createElement('div');
+        clear.classList.add('tag','highlight');
+        clear.id = 'clear';
+        clear.innerText = 'Limpar X';
+        clear.addEventListener('click', () =>{
+            selectedGenre = [];
+            setGenre();
+
+            getMovies(API_URL);
+
+        })
+        tagsEl.append(clear);
+    }
+    
+
+}
 
 getMovies(API_URL);
 
 function getMovies(url){
-    
+    lastUrl = url;
     fetch(url).then(res => res.json()).then(data =>{
         
+        if(data.results.length !== 0){
+            showMovies(data.results);
+             currentPage =  data.page;
+             nextPage = currentPage +1;
+             prevPage =  currentPage -1;
+             totalPages= data.total_pages;
+            current.innerText = currentPage;
+             if(currentPage <= 1){
+                 prev.classList.add('disabled');
+                 next.classList.remove('disabled')
+             }else if(currentPage >= totalPages){
+                prev.classList.remove('disabled');
+                next.classList.add('disabled')
+             }else{
+                prev.classList.remove('disabled');
+                next.classList.add('disabled')
+             }
+
+             tagsEl.scrollIntoView({behavior : 'smooth'})
+        }else{
+            main.innerHTML= '<h1 class="no-results">Sem Resultados Encontrados</h1>'
+        }
         
-        showMovies(data.results);
     })
 
 }
@@ -30,7 +213,7 @@ main.innerHTML= '';
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
-        <img src="${IMG_URL+poster_path}" alt="${title}">
+        <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580"}" alt="${title}">
 
                 <div class="movie-info">
                     <h3>${title}</h3>
@@ -64,7 +247,8 @@ form.addEventListener('submit', (e)=> {
     e.preventDefault();
 
 const searchTerm = search.value;
-
+selectedGenre = [];
+highlightSelect()
 if(searchTerm){
     getMovies(searchURL+'&query='+searchTerm);
 }else{
@@ -72,3 +256,34 @@ if(searchTerm){
 }
 
 })
+
+prev.addEventListener('click',() =>{
+    if(prevPage> 0){
+        pageCall(prevPage);
+    }
+})
+
+
+next.addEventListener('click',() =>{
+    if(nextPage <= totalPages){
+        pageCall(nextPage);
+    }
+})
+
+
+function pageCall(page){
+    let urlSplit = lastUrl.split('?');
+    let queryParams = urlSplit[1].split('&');
+    let key = queryParams[queryParams.length - 1].split('=');
+    if(key[0] != 'page'){
+        let url = lastUrl + '&page='+page;
+        getMovies(url);
+    }else{
+        key[1] = page.toString()
+        let a = key.join('=');
+        queryParams[queryParams.length -1] = a;
+        let b = queryParams.join('&');
+        let url =urlSplit[0] +'?'+ b;
+        getMovies(url);
+    }
+}
