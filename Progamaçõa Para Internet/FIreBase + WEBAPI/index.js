8e9bb118fd2548d9654678c162230c269822c013
Vinicius Rooosevelt <84464600+ViniciusRoosevelt@users.noreply.github.com>
2022-06-02 15:03:52 -0300

    Add files via upload

--- Progamaçõa Para Internet/FIreBase + WEBAPI/index.js
+++ Progamaçõa Para Internet/FIreBase + WEBAPI/index.js
@@ -1,51 +1,77 @@
-const express = require('express')
+// import {query,orderBy,limit} from "firebase/firestore";
+const express = require('express');
+const cors = require('cors');
+const Post = require('./config');
 const app = express()
-
-
-//response as Json
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true }));
 const port = 3000
-// Configurando Firebase
-var admin = require("firebase-admin");
-var serviceAccount = require("./web-api-fire-firebase-adminsdk-1mmwn-e4c8e189d2.json");
-
-admin.initializeApp({
-    credential:
-    admin.credential.cert(serviceAccount)
-})
- 
+//response as Json
+app.use(express.json()); // BoryParser - Transformar array em json
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 
 
+// Receive Posts
+app.get("/" , async(req,res) => {
+    const snapshot = await Post.get();
+  //  const ids = snapshot.docs.map((docs) =>docs.id) // Pegar os ids
+  //  console.log(ids);
+    const list = snapshot.docs.map((doc) =>({id: doc.id, ...doc.data() })) // Map para tratamento e encontar os dados
+    res.send(list);
+});
+
+
+// Pegando pelo timestamp
+app.get("/timestamp" , async(req,res) => {
+    const snapshot = await Post.orderBy('timestamp', 'asc   ').get()
+  //  const ids = snapshot.docs.map((docs) =>docs.id) // Pegar os ids
+  //  console.log(ids);
+    const list = snapshot.docs.map((doc) =>({id: doc.id, ...doc.data() })) // Map para tratamento e encontar os dados
+    res.send(list);
+});
 
 
 
-const db = admin.firestore()
 
-/*
-db.collection('posts').add({
-    text: "Nova Postagem",
-    likes: 0
-})
-.then((docRef) =>{
-    console.log('Document written with id:',docRef.id)
-})
-.catch((error) => {
-    console.error('Error:',error)
-}) */
+// Create Post
+app.post('/create',async (req,res)=>    {
+    let timestamp = new Date()
+    const data = req.body; // Recebe a requisição http
+   await Post.add({data,
+    timestamp
+                  })                // Manda para o firebase
+    res.send({msg:"Post Added"})
+});
 
 
+app.put('/update',async (req,res)=>    {
+    const id = req.body.id;
+    console.log("Before deleting ID",req.body)
+    delete req.body.id
+    console.log("After deleting ID",req.body)
+    const data = req.body
+    await Post.doc(id).update(data);
+   res.send({msg:"Updated"})
+});
 
-db.collection("posts").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
- });
- });
-
-
-
- app.listen(port, () => {
-        console.log('Servidor rodando')
-})
+// Delete Posts pelo id
+app.delete('/delete',async (req,res)=>    {
+    const id = req.body.id;
     
+    await Post.doc(id).delete();
+   res.send({msg:"Deleted"})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Subiu e Rodando em ${port}`))
